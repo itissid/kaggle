0330d9b57f988ad8e3943bec09be773b5545ed2e
Sidharth Gupta <itissid@gmail.com>
2017-10-08 11:35:27 -0400

    Added utilities for PCR from vtreat blogs

---
+++ common-utils.R
@@ -0,0 +1,78 @@
+checkArgs <- function(frame,xvar,yvar,title,...) {
+  args <- list(...)
+  if(missing(frame)||(!is.data.frame(frame))||(nrow(frame)<0)||(ncol(frame)<=0)) {
+    stop("frame must be a non-empty data frame")
+  }
+  if(missing(title)||(!is.character(title))||(length(title)!=1)) {
+    stop("title must be a length 1 character vector")
+  }
+  if(missing(xvar)||(!is.character(xvar))||(length(xvar)!=1)) {
+    stop("xvar must be a length 1 character vector")
+  }
+  if(missing(yvar)||(!is.character(yvar))||(length(yvar)!=1)) {
+    stop("yvar must be a length 1 character vector")
+  }
+  if(!(xvar %in% colnames(frame))) {
+    stop("xvar must be the name of a column in frame")
+  }
+  if(!(yvar %in% colnames(frame))) {
+    stop("yvar must be the name of a column in frame")
+  }
+  if(length(args)!=0) {
+    nm <- names(args)
+    if(length(nm)>0) {
+      stop(paste("unexpected named arguments",paste(nm,collapse = ', ')))
+    }
+    stop(paste("saw",length(args),"unexpected argument values: ",paste(args,collapse=', ')))
+  }
+}
+# Curry without leaking
+padToK <- function(k) {
+  force(k)
+  function(x) {
+    xres <- as.character(x)
+    xres[is.na(xres)] <- 'NA'
+    nx <- length(x)
+    for(i in seq_len(nx)) {
+      di <- k - nchar(xres[[i]])
+      if(di>0) {
+        pi <- paste(rep('_',di), collapse = '')
+        xres[[i]] <- paste(pi,xres[[i]])
+      }
+    }
+    xres
+  }
+}
+
+# get the y lables from a ready to go ggplot2
+getYLabs <- function(p) {
+  info <- ggplot2::ggplot_build(p)
+  origlabs <- info$panel$ranges[[1]]$y.labels  # worked prior to ggplot2.2.0
+  if(!is.null(origlabs)) {
+    return(origlabs)
+  }
+  origlabs <- info$layout$panel_ranges[[1]]$y.labels
+  origlabs
+}
+
+# assumes p1 and p2 have not set scale_y_*
+designYLabelPadFunction <- function(p1,p2) {
+  origlabs1 <- getYLabs(p1)
+  origlabs2 <- getYLabs(p2)
+  lengthTarget <- max(nchar(c(origlabs1,origlabs2)))
+  padToK(lengthTarget)
+}
+
+
+cdeviance <- function(truth,pred,epsilon=1.e-6) {
+  pred = pmax(pred, epsilon)
+  pred = pmin(pred, 1-epsilon)
+  S = 0.0 # assumed log-likelihood of saturated model
+  -2*(sum(ifelse(truth,log(pred),log(1-pred)))-S)
+}
+
+# this function counts logical as discrete
+isDiscrete = function(x) {
+  return (is.factor(x) || is.character(x) || is.integer(x) || is.logical(x))
+}
+
--- plottingUtils.R
+++ plottingUtils.R
@@ -110,3 +110,199 @@ plotPercentileWrapper = function(data, var, plot_title, grid_lines_interactive=2
         data, !!var, type, xlimit=NULL, grid_lines_interactive = grid_lines_interactive, grid_ymax = grid_ymax, plot_title=plot_title)
     ggiraph(code = {print(gg)}, hover_css = "stroke:red;")
 }
+
+
+############ FROM VTREAT #################################
+#' Plot a conditional scatter plot with marginals.  xvar is the independent variable (input or model), and yvar is the dependent variable, and cvar is the condition code
+#'
+#' @param frame data frame to get values from
+#' @param xvar name of the independent (input or model) column in frame
+#' @param yvar name of the dependent (output or result to be modeled) column in frame
+#' @param cvar name of condition variable
+#' @param title title to place on plot
+#' @param ...  no unnamed argument, added to force named binding of later arguments.
+#' @param annot_size numeric scale annotation text (if present)
+#' @param colorPalette name of a Brewer palette (see http://colorbrewer2.org/ )
+#' @param adjust_x  numeric adjust x density plot
+#' @param adjust_y  numeric adjust y density plot
+#' @examples
+#'
+#' set.seed(34903490)
+#' frm = data.frame(x=rnorm(50),y=rnorm(50))
+#' frm$cat <- frm$x+frm$y>0
+#' WVPlots::ScatterHistC(frm, "x", "y", "cat",
+#'                       title="Example Conditional Distribution")
+#'
+#' @export
+ScatterHistC = function(frame, xvar, yvar, cvar, title, ...,
+                        annot_size=3,
+                        colorPalette="Dark2",
+                       adjust_x = 1,
+                       adjust_y = 1) {
+  checkArgs(frame=frame,xvar=xvar,yvar=yvar,title=title,...)
+  minimal_labels = TRUE
+
+  # Use this plot to print the legend.
+  if(is.factor(frame[[cvar]])) {
+    labs = levels(frame[[cvar]])
+    labs = factor(labs,
+                  levels=labs,
+                  labels=paste(" ", labs, sep='')  # add a blank to the front
+    )
+    # this preserves the factor order, if it's not alphabetical
+  } else {
+    # if frame[[cvar]] isn't a factor, then it will sort alphabetical anyway
+    labs = levels(as.factor(frame[[cvar]]))
+    labs = paste(" ", labs, sep='')  # add a blank to the front
+  }
+  nlab = length(labs)
+
+  legendframe = data.frame(x=0, y=seq_len(nlab), cvar=labs)
+  emptyframe = data.frame(x=c(-0.01,1), y=c(0,nlab+2))
+
+  legendplt =  ggplot2::ggplot() +
+    ggplot2::annotate("text", x=0.1, y=nlab+1, label=cvar, size=annot_size) +
+    ggplot2::geom_point(data=legendframe, ggplot2::aes(x=x,y=y,color=cvar),
+                        size=3) +
+    ggplot2::geom_text(data=legendframe, ggplot2::aes(x=x,y=y,label=cvar),
+                       nudge_x=0.025, hjust="left", size=annot_size) +
+    ggplot2::geom_point(data=emptyframe, ggplot2::aes(x=x,y=y),
+                        colour = "white") +
+    ggplot2::theme(plot.background = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   axis.title.x = ggplot2::element_blank(),
+                   axis.title.y = ggplot2::element_blank(),
+                   axis.text.x = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank(),
+                   plot.margin = grid::unit(c(1, 1, 0, 0), "lines"),
+                   legend.position="none") +
+   ggplot2:: scale_color_brewer(palette=colorPalette)
+
+
+  # scatterplot of x and y
+  plot_center = ggplot2::ggplot(frame,
+                                ggplot2::aes_string(x=xvar,y=yvar,color=cvar)) +
+    ggplot2::geom_point() +
+    ggplot2::theme(plot.margin = grid::unit(c(0, 0, 0, 0), "lines")) +
+    ggplot2::scale_color_brewer(palette=colorPalette) +
+    ggplot2::scale_fill_brewer(palette=colorPalette)
+
+  # get the data range, to help align plots
+  x = frame[[xvar]]
+  y = frame[[yvar]]
+  xlims =  c(min(x), max(x))
+  ylims =  c(min(y), max(y))
+
+  #  print(xlims)
+  # print(ggplot_build(plot_center)$panel$ranges[[1]]$x.range)
+
+  plot_center = plot_center +
+    ggplot2::coord_cartesian(xlim=xlims) +
+    ggplot2::scale_x_continuous(expand = c(0,0)) +
+    ggplot2::theme(legend.position="none")
+
+  # print(ggplot_build(plot_center)$panel$ranges[[1]]$x.range)
+
+  # marginal density of x - plot on top
+  #
+  # 0,0,0,0 -- title squooshed down
+  # 1,0,0,0 -- title has space
+  # 0,1,0,0 -- right side is shorter
+  # 0,0,1,0 -- bottom gap bigger
+  # 0,0,0,1 -- left side is shorter
+  #
+  plot_top <- ggplot2::ggplot(frame,
+                              ggplot2::aes_string(x=xvar,color=cvar)) +
+    ggplot2::geom_line(stat='density',adjust=adjust_x) +
+    ggplot2::coord_cartesian(xlim=xlims) +
+     ggplot2::scale_x_continuous(expand = c(0,0))
+  if(minimal_labels) {
+    plot_top = plot_top +
+      ggplot2::theme(legend.position = "none",
+                     axis.title.x = ggplot2::element_blank(),
+                     axis.text.x = ggplot2::element_blank(),
+                     axis.ticks.x = ggplot2::element_blank(),
+                     plot.margin = grid::unit(c(1, 0, 0, 0), "lines"))
+  } else {
+    plot_top = plot_top +
+      ggplot2::theme(plot.margin = grid::unit(c(1, 0, 0, 0), "lines"))
+  }
+  plot_top <- plot_top + ggplot2::scale_color_brewer(palette=colorPalette) +
+    ggplot2::scale_fill_brewer(palette=colorPalette)
+
+
+  # marginal density of y - plot on the right
+  plot_right <- ggplot2::ggplot(frame,
+                                ggplot2::aes_string(x=yvar,color=cvar)) +
+    ggplot2::geom_line(stat='density', adjust=adjust_y) +
+    ggplot2::coord_cartesian(xlim=ylims) +
+    ggplot2::scale_x_continuous(expand = c(0,0)) +
+    ggplot2::coord_flip(xlim=ylims, expand=0)
+  if(minimal_labels) {
+    plot_right = plot_right +
+      ggplot2::theme(legend.position = "none",
+                     axis.title.y = ggplot2::element_blank(),
+                     axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank(),
+                     plot.margin = grid::unit(c(0, 1, 0, 0), "lines"))
+  } else {
+    plot_right = plot_right +
+      ggplot2::theme(plot.margin = grid::unit(c(0, 1, 0, 0), "lines"))
+  }
+  plot_right <- plot_right + ggplot2::scale_color_brewer(palette=colorPalette) +
+    ggplot2::scale_fill_brewer(palette=colorPalette)
+
+  # esimate size
+  yPadFn <- designYLabelPadFunction(plot_center +
+                                      ggplot2::scale_y_continuous(limits=ylims, expand = c(0,0)),
+                                    plot_top)
+  # adjust using estimte
+  plot_center <- plot_center +
+    ggplot2::scale_y_continuous(limits=ylims, label=yPadFn, expand = c(0,0))
+  plot_top <- plot_top +
+    ggplot2::scale_y_continuous(label=yPadFn)
+
+  # arrange the plots together, with appropriate height and width
+  # for each row and column
+
+  gridExtra::grid.arrange(plot_top, legendplt, plot_center, plot_right,
+                          top=grid::textGrob(title),
+                          ncol = 2, nrow = 2, widths = c(4,1), heights = c(1, 4))
+}
+
+ScatterHistN = function(frame, xvar, yvar, zvar, title, ...,
+                        annot_size=3,
+                        colorPalette="RdYlBu",
+                        nclus=3,
+                        adjust_x = 1,
+                        adjust_y = 1) {
+  checkArgs(frame=frame,xvar=xvar,yvar=yvar,title=title,...)
+  q <- sort(unique(quantile(frame[[zvar]],seq(0, 1, 1/nclus))))
+  yC <- cut(frame[[zvar]],q,include.lowest=TRUE)
+  if(length(unique(yC))<=1) {
+    q <- sort(unique(c(q,median(unique(frame[[zvar]])))))
+    yC <- cut(frame[[zvar]],q,include.lowest=TRUE)
+  }
+  frame[[zvar]] <- yC
+  ScatterHistC(frame, xvar, yvar, zvar, title, ...,
+               annot_size=annot_size,
+               colorPalette=colorPalette,
+               adjust_x = adjust_x,
+               adjust_y = adjust_y)
+}
+extractProjection <- function(ndim,princ) {
+  # pull off the rotation.  
+  proj <- princ$rotation[,1:ndim] 
+  # sign was arbitrary, so flip in convenient form
+  for(i in seq_len(ndim)) {
+    si <- sign(mean(proj[,i]))
+    if(si!=0) {
+      proj[,i] <- proj[,i]*si
+    }
+  }
+  proj
+}
