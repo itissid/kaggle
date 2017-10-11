cabe1b1994cf69bd004ee4e2a7bdcac4c50cec43
Sidharth Gupta <itissid@gmail.com>
2017-10-11 15:00:07 -0400

    Added 2 utilities to join the data with 2017.
    The merge routine returns merged data from 2016 and 2017 and replaces
    NA values in 2016 with those of 2017.

--- utils.R
+++ utils.R
@@ -77,6 +77,69 @@ readAndTransformData = function(pr='inputs/properties_2016.csv', tr = 'inputs/tr
     return(list(transactions %>% dplyr::left_join(properties, by="id_parcel"), properties))
 }
 
+source("datadefs.R")
+merge2017Data = function(tr.2016, pr.2016, pr.2017, join_features=join_features.default) {
+    # The only feature that actually comes up is quality. but its only ~ 1.2% of the cases
+    # tr.X transaction data set for year X
+    # pr.x properties data set for year X
+
+    tr.join = tr.2016 %>% dplyr::inner_join(pr.2017 %>% dplyr::select(c(join_features, "id_parcel")),
+                                            by="id_parcel")
+    pr.join = pr.2016 %>% dplyr::inner_join(pr.2017 %>% dplyr::select(c(join_features, "id_parcel")),
+                                            by="id_parcel")
+    print(".")
+    assertthat::assert_that(nrow(tr.join) == nrow(tr.2016)) # the rows are the same
+    assertthat::assert_that(nrow(pr.join) == nrow(pr.2016)) # the rows are the same
+    join_features.left = sapply(join_features, function(f){paste(f, ".x", sep="")})
+    join_features.right = sapply(join_features, function(f){paste(f, ".y", sep="")})
+    for(i in 1:length(join_features.left)) {
+        feature.x = join_features.left[[i]]
+        feature.y = join_features.right[[i]]
+        feature.newdata = paste(join_features[[i]], "_new", sep="")
+        feature.diffdata = paste(join_features[[i]], "_diff", sep="")
+        feature.x.name = quo(!!as.name(feature.x))
+        feature.y.name = quo(!!as.name(feature.y))
+        tr.join %<>% dplyr::mutate(
+                         !!feature.newdata := is.na(!!as.name(feature.x)) & !is.na(!!as.name(feature.y))) %>%
+                       dplyr::mutate(
+                         !!feature.diffdata := !is.na(!!feature.x.name) & !is.na(!!feature.y.name) & as.character(!!feature.x.name) != as.character(!!feature.y.name))
+        pr.join %<>% dplyr::mutate(
+                         !!feature.newdata := is.na(!!as.name(feature.x)) & !is.na(!!as.name(feature.y))) %>%
+                       dplyr::mutate(
+                         !!feature.diffdata := !is.na(!!feature.x.name) & !is.na(!!feature.y.name) & as.character(!!feature.x.name) != as.character(!!feature.y.name))
+    }
+    print("..")
+    # The missing values
+    missing.2016 = tr.join %>% dplyr::select_at(vars(ends_with("_new"))) %>% summarize_all(funs(sum(.))) %>% tidyr::gather() %>% arrange(value)
+    diff.vals.2016 = tr.join %>% dplyr::select_at(vars(ends_with("_diff"))) %>% summarize_all(funs(sum(.))) %>% tidyr::gather() %>% arrange(value)
+    pr.missing.2016 = pr.join %>% dplyr::select_at(vars(ends_with("_new"))) %>% summarize_all(funs(sum(.))) %>% tidyr::gather() %>% arrange(value)
+    pr.diff.vals.2016 = pr.join %>% dplyr::select_at(vars(ends_with("_diff"))) %>% summarize_all(funs(sum(.))) %>% tidyr::gather() %>% arrange(value)
+    print("...")
+    for(i in 1:length(join_features.left)) {
+        left = join_features.left[[i]]
+        right = join_features.right[[i]]
+        tr.join %<>%
+            dplyr::mutate(!!left := ifelse(is.na(!!as.name(left)), !!as.name(right), !!as.name(left)))
+        pr.join %<>%
+            dplyr::mutate(!!left := ifelse(is.na(!!as.name(left)), !!as.name(right), !!as.name(left)))
+    }
+    return(list(tr.join, pr.join, missing.2016, diff.vals.2016, pr.missing.2016, pr.diff.vals.2016))
+}
+
+processJoint1617Data = function(X,
+                                join_features=join_features.default,
+                                non_join_features=non_join_features.default) {
+    join_features.left = sapply(join_features, function(f) {paste(f, ".x", sep="")})
+    for(i in 1:length(join_features.left)) {
+        old = join_features[[i]]
+        left = join_features.left[[i]]
+        print(old)
+        print(left)
+        X %<>% dplyr::rename_(.dots=setNames(left, old))
+    }
+    X %<>% select_at(vars(c(join_features, non_join_features)))
+}
+
 
 prepareData = function(
                        recode_chars=T,
